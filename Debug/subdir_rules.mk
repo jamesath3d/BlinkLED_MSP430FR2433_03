766c1e39682911967596223e003179c72f6799eb
Your Name <you@example.com>
2021-10-01 12:11:45 -0400

    cmdc

--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -2,13 +2,13 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-SHELL = cmd.exe
+#SHELL = cmd.exe
 
 # Each subdirectory must supply rules for building sources it contributes
 %.o: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: GNU Compiler'
-	"C:/ti/ccs1040/ccs/tools/compiler/msp430-gcc-9.3.1.11_win64/bin/msp430-elf-gcc-9.3.1.exe" -c -mmcu=msp430fr2433 -mhwmult=f5series -I"C:/ti/ccs1040/ccs/ccs_base/msp430/include_gcc" -I"C:/devJames/workspace_v10/BlinkLED_MSP430FR2433_03" -I"C:/devJames/workspace_v10/BlinkLED_MSP430FR2433_03/driverlib/MSP430FR2xx_4xx" -I"C:/ti/ccs1040/ccs/tools/compiler/msp430-gcc-9.3.1.11_win64/msp430-elf/include" -Og -g -gdwarf-3 -gstrict-dwarf -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	"/home/ti/ti/ccs1040/ccs/tools/compiler/msp430-gcc-9.3.1.11_win64/bin/msp430-elf-gcc-9.3.1.exe" -c -mmcu=msp430fr2433 -mhwmult=f5series -I"/home/ti/ti/ccs1040/ccs/ccs_base/msp430/include_gcc" -I"C:/devJames/workspace_v10/BlinkLED_MSP430FR2433_03" -I"C:/devJames/workspace_v10/BlinkLED_MSP430FR2433_03/driverlib/MSP430FR2xx_4xx" -I"/home/ti/ti/ccs1040/ccs/tools/compiler/msp430-gcc-9.3.1.11_win64/msp430-elf/include" -Og -g -gdwarf-3 -gstrict-dwarf -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-SHELL = cmd.exe
+#SHELL = cmd.exe
 
 # Add inputs and outputs from these tool invocations to the build variables 
 LD_SRCS += \
