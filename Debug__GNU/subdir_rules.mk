183dcfdcd6665f6a5a346a11d8a9cf1c15397826
Your Name <you@example.com>
2021-10-01 13:43:01 -0400

    win64 to linux64

--- Debug__GNU/subdir_rules.mk
+++ Debug__GNU/subdir_rules.mk
@@ -8,7 +8,7 @@
 %.o: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: GNU Compiler'
-	"/home/ti/ti/ccs1040/ccs/tools/compiler/msp430-gcc-9.3.1.11_linux64/bin/msp430-elf-gcc-9.3.1" -c -mmcu=msp430fr2433 -mhwmult=f5series -I"/home/ti/ti/ccs1040/ccs/ccs_base/msp430/include_gcc" -I"/home/dyn/workspace_v10/BlinkLED_MSP430FR2433_03" -I"/home/dyn/workspace_v10/BlinkLED_MSP430FR2433_03/driverlib/MSP430FR2xx_4xx" -I"/home/ti/ti/ccs1040/ccs/tools/compiler/msp430-gcc-9.3.1.11_win64/msp430-elf/include" -Og -g -gdwarf-3 -gstrict-dwarf -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	"/home/ti/ti/ccs1040/ccs/tools/compiler/msp430-gcc-9.3.1.11_linux64/bin/msp430-elf-gcc-9.3.1" -c -mmcu=msp430fr2433 -mhwmult=f5series -I"/home/ti/ti/ccs1040/ccs/ccs_base/msp430/include_gcc" -I"/home/dyn/workspace_v10/BlinkLED_MSP430FR2433_03" -I"/home/dyn/workspace_v10/BlinkLED_MSP430FR2433_03/driverlib/MSP430FR2xx_4xx" -I"/home/ti/ti/ccs1040/ccs/tools/compiler/msp430-gcc-9.3.1.11_linux64/msp430-elf/include" -Og -g -gdwarf-3 -gstrict-dwarf -Wall -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
