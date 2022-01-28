e721fdf0dc039eb046ed82bb1b34600b48d1878c
Your Name <you@example.com>
2022-01-28 13:04:46 -0500

    x

--- _clk_init.h
+++ _clk_init.h
@@ -18,15 +18,20 @@
 */
 
 #define _mclk_to_32768  CSCTL4 = ( CSCTL4 & (~(SELMS0 | SELMS1 | SELMS2 ))) | SELMS1 ; // set mclk & smclk to 32768 Hz
+
 #define _divm_to_2      CSCTL5 = ( CSCTL5 & (~(DIVM2 | DIVM1 | DIVM0 ))) | SELMS0 ;          // set DIVM to 2 ==> 16384
 #define _divm_to_4      CSCTL5 = ( CSCTL5 & (~(DIVM2 | DIVM1 | DIVM0 ))) | SELMS1 ;          // set DIVM to 4 ==>  8192
 #define _divm_to_8      CSCTL5 = ( CSCTL5 & (~(DIVM2 | DIVM1 | DIVM0 ))) | SELMS1 | SELMS0 ; // set DIVM to 8 ==>  4096
+
 #define _aclk_to_32768  CSCTL4 =   CSCTL4 |    SELA                               ; // set Aclk to 32768 
 
 #define _clk_to_32768  { _mclk_to_32768  ;                ; _aclk_to_32768  ; }
 #define _clk_to_16384  { _mclk_to_32768  ; _divm_to_2     ; _aclk_to_32768  ; }
 #define _clk_to_8192   { _mclk_to_32768  ; _divm_to_4     ; _aclk_to_32768  ; }
 #define _clk_to_4096   { _mclk_to_32768  ; _divm_to_8     ; _aclk_to_32768  ; }
+
+/* CSCTL6 Control Bits */
+#define _trun_off_xt1 CSCTL6 = (CSCTL6 & (~( XT1DRIVE0 | XT1DRIVE1 ))) | XT1AGCOFF  ;
 
 void _clk_init_1mhz(void);
 
