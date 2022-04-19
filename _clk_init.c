9bf4ce0bf6ebe983bcaa9bdd7f48b93d6ac11e31
Your Name <you@example.com>
2022-04-19 09:22:33 -0400

    x

--- _clk_init.c
+++ _clk_init.c
@@ -86,33 +86,7 @@ void _clk_init_8mhz( void ___gpio_init(void) ){
         CSCTL5 = DIVM__1;
         //end code
     }
-    if (0){
-        //Configure one FRAM wait state
-        FRCTL0 = FRCTLPW + NWAITS_1;
-        //disable FLL
-        __bis_SR_register(SCG0);
-        //Set REFO (32.768khz LFO)as FLL reference source
-        CSCTL3 |= SELREF__REFOCLK;
-        //clear DCO and MOD registers
-        CSCTL0 = 0;
-        //clear DCO frequency select bits first
-        CSCTL1 &= ~(DCORSEL_7);
-        //set DCO = 16MHz
-        CSCTL1 = DCOFTRIMEN + DCOFTRIM0 + DCOFTRIM1 + DCORSEL_5;
-        //DCOCLKDIV = 16MHz
-        CSCTL2 = FLLD_0 + 487;
-        __delay_cycles(5);
-        //enable FLL
-        __bic_SR_register(SCG0);
-        //poll until FLL is locked
-        while(CSCTL7 & (FLLUNLOCK0 + FLLUNLOCK1));
-        //set default REFO(~32768Hz) as ACLK source, ACLK = 32768Hz
-        //default DCODIV as MCLK and SMCLK source
-        CSCTL4 = SELMS__DCOCLKDIV + SELA__REFOCLK;
-        //divide MCLK by 1
-        CSCTL5 = DIVM__1;
-        //end code
-    }
+
     if ( ___gpio_init ) {
         ___gpio_init();
     }
